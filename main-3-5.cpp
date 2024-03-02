23333d5ea9f9e3db20328a7783d54c8b78f4bc5e
Tri Hai <haitri62@gmail.com>
2024-03-03 01:59:21 +1030

    Tricky question

--- main-3-5.cpp
+++ main-3-5.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 extern double sum_even(double array[], int n);
 int main(){
-    double array[10] = {56.789, 23.456, 78.901, 12.345, 45.678, 89.012, 34.567, 67.890, 90.123, 23.456};
+    double array[10] = {1,2,3,4,5,6,7,8,9,10};
     std::cout << sum_even(array, 10) << std::endl;
     return 0;
 
 }
+//  Write a function that returns the sum of the elements in 
+//the even positions in an array. 
+// The function must return 0 if the size parameter, n, is less than 1. 
+//Note, 0 is considered an even position.
