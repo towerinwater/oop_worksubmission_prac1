e2b894b1bd7f9ac0400ee541a88bf9c6b152ef23
Tri Hai <haitri62@gmail.com>
2024-03-03 01:06:39 +1030

    Medium Problem

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 void two_five_nine(int array[], int n){
-    int count_arr[3];
+    int count_arr[3] = {0};
     for(int i = 0; i < n; i++){
         switch(array[i]){
             case 2:
-                count_arr[0]++;
+                count_arr[0] += 1;
                 break;
             case 5:
-                count_arr[1]++;
+                count_arr[1] += 1;
                 break;
             case 9:
-                count_arr[2]++;
+                count_arr[2] += 1;
                 break;
         }
     }
-    std::cout << "2: " << count_arr[0]
-                << ";5: " << count_arr[1]
-                << ";9: " << count_arr[2] << std::endl;
+    std::cout << "2:" << count_arr[0]
+                << ";5:" << count_arr[1]
+                << ";9:" << count_arr[2] << std::endl;
 }
